f0cf7b7be89ae80d085be478277ef7d5d3ff7de7
tusharsharma045 <tusharsharma02007@gmail.com>
2026-08-20 12:50:33 +0530

    initial commit

--- day-4/index.js
+++ day-4/index.js
@@ -1,14 +1,48 @@
+const http = require("http") //when client and server communicate, http module is used.
+
 const server = http.createServer((req,res)=>{
 
-    if (req.method === 'GET'){
-        
-        res.write('<h1>Hello World</h1>')
-            res.end();
-        }
-    else if (req.method === 'POST'){
-        res.write('<h1>Hello World</h1>')
-            res.end();
-    }
-});
+    // res.write("<h1>Hello World</h1>")
+    // res.end()
 
-ser
+//     if(req.url ==="/"){
+//         res.write("<h1>Home Page</h1>")
+//         res.end()
+//     }   
+
+//     console.log(req.url)
+//     res.end()
+
+    // console.log(req.method)
+    
+
+    if (req.method === "GET"){
+        res.write("<h1>GET Request</h1>")
+        res.end()
+    }
+
+    if(req.method === "POST"){
+        res.write("<h1>POST Request</h1>")
+        res.end()
+    }
+
+    if(req.method === "PUT"){
+        res.write("<h1>PUT Request</h1>")
+        res.end()
+    }
+
+    if(req.method === "DELETE"){
+        res.write("<h1>DELETE Request</h1>")
+        res.end()
+    }
+    
+
+
+})
+
+
+
+
+server.listen(3000,()=>{
+    console.log("Server is running on PORT 3000")
+})
